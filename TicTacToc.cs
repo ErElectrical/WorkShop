28f87ab469711299e96655d58fc47868a05aea7b
ErElectical <mohittanwar786.mt@gmail.com>
2022-02-14 12:56:40 +0000

    [Add]Mohit position Array added

---
+++ TicTacToc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacTocProblem
+{
+    public class TicTacToc
+    {
+        public static void Position()
+        {
+            char[] posarr = new char[10];
+            for (int i = 0; i < posarr.Length; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                posarr[i] = ' ';
+            }
+            Console.WriteLine("Array initialise");
+        }
+    }
+     
+}
