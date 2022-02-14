789d94dc16506179489bed36ebf34493fc9459d7
ErElectical <mohittanwar786.mt@gmail.com>
2022-02-14 12:54:25 +0000

    Main Method TicTacToe

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicTacTocProblem
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome To TicTacToc game ");
+            TicTacToc.Position();
+  
+            
+            
+        }
+    }
+}
